7ac216f62f527e27a3dd289a6c9497429d4d919f
Connor Lopshire <lopshire.connor@gmail.com>
2022-09-12 10:49:40 -0500

    viewset for login and register

--- nationalparkapi/serializers/park.py
+++ nationalparkapi/serializers/park.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 
 from nationalparkapi.models.parks import Park
+from nationalparkapi.serializers.address import AddressSerializer
 from nationalparkapi.serializers.image import ImageSerializer
+from nationalparkapi.serializers.review import ReviewSerializer
 
 class ParkSerializer(serializers.ModelSerializer):
     """JSON serializer for main park list and bucket list"""
     images = ImageSerializer(many=True)
     class Meta:
         model = Park
-        fields = ('id', 'images', 'full_name')
+        fields = ('id', 'images', 'full_name', 'state')
+
+class DetailedParkSerializer(serializers.ModelSerializer):
+    """serializer for detailed park view including images and address and reviews"""
+    images = ImageSerializer(many=True)
+    address = AddressSerializer()
+    reviews = ReviewSerializer(many = True)
+    class Meta:
+        model = Park
+        fields = ('id', 'images', 'address', 'reviews', 'url', 'full_name', 'description', 'designation')  
